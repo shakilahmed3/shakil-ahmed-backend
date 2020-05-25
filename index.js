04ac0a6f3efa8b0f6289bac97c3c94ff2f6e2024
Shakil Ahmed <shakilatrai5@gmail.com>
2020-05-25 23:42:55 +0600

    editing done

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 
 const transporter = nodemailer.createTransport({
