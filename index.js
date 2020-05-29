d911f0cca23467f2076dc8c07bbc1fed18c9240c
Shakil Ahmed <shakilatrai5@gmail.com>
2020-05-29 19:51:11 +0600

    Successfully done gmail intregating

--- index.js
+++ index.js
@@ -1,38 +1,40 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const nodemailer = require('nodemailer');
+
+const sgMail = require('@sendgrid/mail');
+
 require('dotenv').config();
 
 const app = express();
+
 app.use(cors());
 app.use(bodyParser.json());
 
-const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    secure:false,
-    port:25,
-    auth: {
-        user: process.env.EMAIL,
-        pass: process.env.PASS
-    }
+
+app.get('/',(req, res)=>{
+    res.send("Welcome email server!");
 })
 
+sgMail.setApiKey(process.env.APIKEY);
+
+
 app.post('/getGmail', (req, res) => {
-    const info = req.body;
-    const mailOptions = {
-        from: `shakilatrai6@gmail.com`,
-        to: 'shakilatrai5@gmail.com',
-        subject: `${info.subject}`,
-        text: `${info.message}\n${info.name}\n${info.email}`
-    }
-    transporter.sendMail(mailOptions, function (err, data) {
-        if (err) {
-            res.send("Error");
-        } else {
-            res.send('Message Sent!!!');
+    const info = req.body;   
+    
+        const msg = {
+            to: 'shakilatrai5@gmail.com',
+            from: `Web info from shakil<cseshakil6@gmail.com>`,
+            subject: `${info.subject}`,
+            text: `${info.message}\n${info.name}\n${info.email}`,
         }
-    })
+
+        sgMail.send(msg).then(() => {
+            console.log('Message sent')
+        }).catch((error) => {
+            console.log(error.response.body)
+        })   
+
 });
 
 const port = process.env.PORT || 4200;
