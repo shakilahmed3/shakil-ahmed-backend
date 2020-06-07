3cb540a30bd95b4e8b6baf28c54cb0322cfbf221
Shakil Ahmed <shakilatrai5@gmail.com>
2020-06-07 14:29:59 +0600

    Readme.md updated

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# shakil-ahmed-backend
+
+## Shakil Ahmed (Personal portfolio website) (Full Stack):
+This is my personal portfolio website. Where customer can view my details and contact me by fill up contact information. And I will get email from the contact form to my gmail. This is full responsive web design. Where All devices supported.<br/><br/>
+
+
+**Technology:** React, Redux, Material-UI, Bootstrap, Node.js, MongoDB<br/>
+**Tools:** VSCode, Git, Netlify, Firebase, Hiroku.<br/>
+**Live Link:** [Shakil Ahmed](https://www.shakilcse.me/ "Shakil Ahmed")<br/>
+**Front-End Repo Link:** [Shakil Ahmed](https://github.com/shakil1206/shakil-ahmed "Shakil Ahmed")<br/>
+
+
+## How to use it?
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+At first clone this project. Then open your terminal and run--> `npm install` After installing you can now run --> `npm start` Then automatically run this project through your browser.
+
+Thank you.
+
+
--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-
+require('dotenv').config();
 const sgMail = require('@sendgrid/mail');
 
-require('dotenv').config();
 
 const app = express();
 
@@ -12,7 +11,8 @@ app.use(cors());
 app.use(bodyParser.json());
 
 
-app.get('/',(req, res)=>{
+
+app.get('/', (req, res) => {
     res.send("Welcome email server!");
 })
 
@@ -20,20 +20,20 @@ sgMail.setApiKey(process.env.APIKEY);
 
 
 app.post('/getGmail', (req, res) => {
-    const info = req.body;   
-    
-        const msg = {
-            to: 'shakilatrai5@gmail.com',
-            from: `Web info from shakil<cseshakil6@gmail.com>`,
-            subject: `${info.subject}`,
-            text: `${info.message}\n${info.name}\n${info.email}`,
-        }
+    const info = req.body;
 
-        sgMail.send(msg).then(() => {
-            console.log('Message sent')
-        }).catch((error) => {
-            console.log(error.response.body)
-        })   
+    const msg = {
+        to: 'shakilatrai5@gmail.com',
+        from: `Web info from shakil<cseshakil6@gmail.com>`,
+        subject: `${info.subject}`,
+        text: `${info.message}\n${info.name}\n${info.email}`,
+    }
+
+    sgMail.send(msg).then(() => {
+        console.log('Message sent')
+    }).catch((error) => {
+        console.log(error.response.body)
+    })
 
 });
 
